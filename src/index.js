9cab35a747b5bdcc153ebb03c7f6b26cda811008
manga <manganellidev@gmail.com>
2023-11-29 11:29:27 -0300

    feat: implement default route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(3333, () => {
   console.log("App is running on port 3333");
 });
